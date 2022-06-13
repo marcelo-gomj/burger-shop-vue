482082795c7496cf3a4d327e2ba220f0f34e0494
marcelo-gomj <marcelomgj005@gmail.com>
2022-06-12 22:37:42 -0300

    add: starting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar :logo="logo_src" :alt="app_name" />
+    <router-view />
+    <Footer />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script> 
+    import NavBar from "../src/components/NavBar.vue";
+    import Footer from "../src/components/Footer.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        NavBar,
+        Footer
+    },
+    data(){
+        return {
+            logo_src: "/img/logo.png",
+            app_name : "Make Your Burger" 
+        }
+    }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
 }
+
+main{
+    display: flex;
+    background-color: rgb(221, 81, 0);
+    height: 100vh;
+    widows: 100%;
+    padding-top: 5%;
+    flex-direction: column;
+    align-items: center;
+}
+.container-input{
+    width: 60%;
+}
+
+input{
+    width: 100%;
+    height: 2rem;
+    background: none;
+    border: 2px solid white;
+}
+
+input:focus{
+    border: none;
+}
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img :src="logo" :alt="alt" id="logo" />
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "NavBar",
+    props : [logo, alt]
+}
+</script>
+
+<style>
+
+</style>
